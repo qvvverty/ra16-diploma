9029c9142d451c33f2a591041768a05cb6e63bd6
Leonid Kostyuchenko <kstchnko@gmail.com>
2021-03-25 17:22:23 +0300

    add about, contacts and some routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Banner from './components/Banner';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import MainContainer from './components/MainContainer';
+import About from './components/About';
+import Contacts from './components/Contacts';
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <MainContainer>
         <Banner />
+        <Route path="/about" component={About} />
+        <Route path="/contacts" component={Contacts} />
       </MainContainer>
       <Footer />
-    </>
+    </Router>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,27 @@
+import { Link, NavLink } from "react-router-dom";
+
 export default function Header() {
   return (
     <header className="container">
       <div className="row">
         <div className="col">
           <nav className="navbar navbar-expand-sm navbar-light bg-light">
-            <a className="navbar-brand" href="/">
+            <Link to="/" className="navbar-brand">
               <img src="/img/header-logo.png" alt="Bosa Noga" />
-            </a>
+            </Link>
             <div className="collapase navbar-collapse" id="navbarMain">
               <ul className="navbar-nav mr-auto">
-                <li className="nav-item active">
-                  <a className="nav-link" href="/">Главная</a>
+                <li className="nav-item">
+                  <NavLink exact to="/" className="nav-link">Главная</NavLink>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/catalog.html">Каталог</a>
+                  <NavLink to="/catalog" className="nav-link">Каталог</NavLink>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/about.html">О магазине</a>
+                  <NavLink to="/about" className="nav-link">О магазине</NavLink>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/contacts.html">Контакты</a>
+                  <NavLink to="/contacts" className="nav-link">Контакты</NavLink>
                 </li>
               </ul>
               <div>
