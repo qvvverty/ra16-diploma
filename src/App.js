6200140783f03673f4ba4e7b69bd6c823a1e09ff
Leonid Kostyuchenko <kstchnko@gmail.com>
2021-03-27 13:46:47 +0300

    add 404 page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Banner from './components/Banner';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import MainContainer from './components/MainContainer';
 import About from './components/About';
 import Contacts from './components/Contacts';
+import Page404 from './components/Page404';
 
 function App() {
   return (
@@ -13,8 +14,12 @@ function App() {
       <Header />
       <MainContainer>
         <Banner />
-        <Route path="/about" component={About} />
-        <Route path="/contacts" component={Contacts} />
+        <Switch>
+          <Route path="/about" component={About} />
+          <Route path="/contacts" component={Contacts} />
+          <Route exact path="/" />
+          <Route path="*" component={Page404} />
+        </Switch>
       </MainContainer>
       <Footer />
     </Router>
